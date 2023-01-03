8afd27182b4e91b8964971767fc78afe4bf7e2b6
safouene <safouene-abbachi@hotmail.fr>
2023-01-03 16:20:42 +0100

    8.10: Adding a book

--- part08/GraphQL_frontend/src/components/NewBook.js
+++ part08/GraphQL_frontend/src/components/NewBook.js
@@ -1,32 +1,38 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { ADD_BOOK, ALL_BOOKS, All_AUTHORS } from '../queries';
+import { useMutation } from '@apollo/client';
 
 const NewBook = (props) => {
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [published, setPublished] = useState('')
-  const [genre, setGenre] = useState('')
-  const [genres, setGenres] = useState([])
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [published, setPublished] = useState('');
+  const [genre, setGenre] = useState('');
+  const [genres, setGenres] = useState([]);
+
+  const [createBook] = useMutation(ADD_BOOK, {
+    refetchQueries: [{ query: ALL_BOOKS }, { query: All_AUTHORS }],
+  });
 
   if (!props.show) {
-    return null
+    return null;
   }
 
   const submit = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
+    createBook({ variables: { title, author, published, genres } });
+    console.log('add book...');
 
-    console.log('add book...')
-
-    setTitle('')
-    setPublished('')
-    setAuthor('')
-    setGenres([])
-    setGenre('')
-  }
+    setTitle('');
+    setPublished('');
+    setAuthor('');
+    setGenres([]);
+    setGenre('');
+  };
 
   const addGenre = () => {
-    setGenres(genres.concat(genre))
-    setGenre('')
-  }
+    setGenres(genres.concat(genre));
+    setGenre('');
+  };
 
   return (
     <div>
@@ -50,7 +56,7 @@ const NewBook = (props) => {
           <input
             type="number"
             value={published}
-            onChange={({ target }) => setPublished(target.value)}
+            onChange={({ target }) => setPublished(+target.value)}
           />
         </div>
         <div>
@@ -66,7 +72,7 @@ const NewBook = (props) => {
         <button type="submit">create book</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default NewBook
+export default NewBook;
--- part08/GraphQL_frontend/src/queries.js
+++ part08/GraphQL_frontend/src/queries.js
@@ -18,3 +18,21 @@ export const ALL_BOOKS = gql`
     }
   }
 `;
+export const ADD_BOOK = gql`
+  mutation createBook(
+    $title: String!
+    $author: String!
+    $published: Int!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      author: $author
+      published: $published
+      genres: $genres
+    ) {
+      title
+      author
+    }
+  }
+`;
