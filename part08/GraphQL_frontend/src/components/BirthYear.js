597f288e5994aa1ef640d86c1516759adc8f78ee
safouene <safouene-abbachi@hotmail.fr>
2023-01-03 19:34:34 +0100

    8.12: Authors birth year advanced

--- part08/GraphQL_frontend/src/components/BirthYear.js
+++ part08/GraphQL_frontend/src/components/BirthYear.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import { UPDATE_AUTHOR, All_AUTHORS } from '../queries';
-import { useMutation } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 
 const BirthYear = () => {
   const [name, setName] = useState('');
   const [born, setBorn] = useState('');
-
+  const { data } = useQuery(All_AUTHORS);
   const [updateBirth] = useMutation(UPDATE_AUTHOR, {
     refetchQueries: [{ query: All_AUTHORS }],
   });
 
   const submit = (e) => {
     e.preventDefault();
-    const setBornTo = +born;
+    const setBornTo = Number(born);
     updateBirth({ variables: { name, setBornTo } });
     setName('');
     setBorn('');
@@ -23,11 +23,18 @@ const BirthYear = () => {
       <form onSubmit={submit}>
         <div>
           name
-          <input
+          <select
             type="text"
             value={name}
             onChange={({ target }) => setName(target.value)}
-          />
+          >
+            <option value="">--Please choose an option--</option>
+            {data.allAuthors.map(({ name }) => (
+              <option key={name} value={name}>
+                {name}
+              </option>
+            ))}
+          </select>
         </div>
         <div>
           born
